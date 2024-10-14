10dc23ce5a7dccdd6fcfa906f78a8f7afcb59b0c
Jakob Farian Krarup <kr@rup.dk>
2024-10-14 11:38:52 +0200

    Improved structure for template and solution

--- TaskParallel/Program.cs
+++ TaskParallel/Program.cs
@@ -5,7 +5,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        ConvertTextFilesToPdf();
+    }
 
+    private static void ConvertTextFilesToPdf()
+    {
         //get full TextFiles subdirectory path
         var textFileFolderPath = Path.Combine(Environment.CurrentDirectory, "TextFiles\\");
 
@@ -19,84 +23,3 @@ class Program
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//static void Main(string[] args)
-//{
-
-//    //get full TextFiles subdirectory path
-//    var textFileFolderPath = Path.Combine(Environment.CurrentDirectory, "TextFiles\\");
-
-//    //get paths of all *.txt files in that directory
-//    var filePaths = Directory.GetFiles(textFileFolderPath, "*.txt");
-
-//    //note the starting time
-//    var begin = DateTime.Now;
-
-//    //run conversions in parallel
-//    Parallel.ForEach(filePaths, (filepath) => PdfTool.TextFileToPdf(filepath));
-
-//    //calculate and show time taken
-//    Console.WriteLine($"Conversion took  {(DateTime.Now - begin).TotalSeconds} seconds");
-//}
---
+++ TaskParallel/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace TaskParallel;
+class Solution
+{
+    public static void ConvertTextFilesToPdfInParallel()
+    {
+        //get full TextFiles subdirectory path
+        var textFileFolderPath = Path.Combine(Environment.CurrentDirectory, "TextFiles\\");
+
+        //get paths of all *.txt files in that directory
+        var filePaths = Directory.GetFiles(textFileFolderPath, "*.txt");
+
+        //note the starting time
+        var begin = DateTime.Now;
+
+        //run conversions in parallel
+        Parallel.ForEach(filePaths, (filepath) => PdfTool.TextFileToPdf(filepath));
+
+        //calculate and show time taken
+        Console.WriteLine($"Conversion took  {(DateTime.Now - begin).TotalSeconds} seconds");
+    }
+}
