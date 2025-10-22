b1c3bd5fce74ba233c4af1ad240091a046cb414f
Andreas-SB <10632087@ucn.dk>
2025-10-23 00:32:32 +0200

    1.0

--- TaskParallel/Program.cs
+++ TaskParallel/Program.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using iText.Kernel.Pdf.Canvas.Parser.ClipperLib;
+using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 namespace TaskParallel;
 class Program
 {
+    static DateTime startTime;
+    static DateTime endTime;
     static void Main(string[] args)
     {
+        //Start time
+        startTime = DateTime.Now;
+
+        //Create thread
+        Thread TimeThread = new Thread(TimeTracker);
+        TimeThread.Start();
+        
+        //Run converter
         ConvertTextFilesToPdf();
+
+        //End time
+        endTime = DateTime.Now;
+
+        //Wait on thread to finish
+        TimeThread.Join();
+
+        //Print total time
+        Console.WriteLine($"\nTotal time taken: {(endTime - startTime).TotalSeconds:F2} seconds");
+    }
+
+    private static void TimeTracker()
+    {
+        while (endTime == default)
+        {
+            var elapsed = DateTime.Now - startTime;
+            Console.Write($"\rTime elapsed: {elapsed.TotalSeconds:F2} seconds");
+            Thread.Sleep(500);
+        }
     }
 
     private static void ConvertTextFilesToPdf()
@@ -17,9 +49,9 @@ class Program
         var filePaths = Directory.GetFiles(textFileFolderPath, "*.txt");
 
         //run conversions in parallel
-        foreach (var filepath in filePaths)
+        Parallel.ForEach(filePaths, filePath =>
         {
-            PdfTool.TextFileToPdf(filepath);
-        }
+            PdfTool.TextFileToPdf(filePath);
+        });
     }
 }
